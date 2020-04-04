26fabec7db2fef49cb7d6d5f6cf2c9d0a4417eb2
Kirill Konshin <kirill-konshin@users.noreply.github.com>
2020-04-03 17:52:08 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 export default ({ foo, pathname }) => <div>Hello {JSON.stringify({ foo, pathname })}.</div>;
 export const getStaticProps = () => {
-  console.error("This should not be called");
+  console.log('Index.getStaticProps');
   return { props: { foo: "bar" } };
 };
